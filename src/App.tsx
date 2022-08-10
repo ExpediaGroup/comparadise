02d5e6ac49ff6d4ec430441f81f9a64cd0cc6104
Dan Adajian <dadajian@expediagroup.com>
2022-08-10 12:17:22 -0500

    address breaking change

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,12 @@ import './App.css';
 import { Main } from './components/Main';
 import { ClientProvider } from './providers/ClientProvider';
 import { QueryParamProvider } from 'use-query-params';
+import { ReactRouter6Adapter } from 'use-query-params/adapters/react-router-6';
 
 function App() {
   return (
     <ClientProvider>
-      <QueryParamProvider>
+      <QueryParamProvider adapter={ReactRouter6Adapter}>
         <Main />
       </QueryParamProvider>
     </ClientProvider>
