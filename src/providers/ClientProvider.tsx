7376e49f3e3cc2118df647ddf12cb646b712a34c
Dan Adajian <dadajian@expediagroup.com>
2022-11-26 21:18:40 -0600

    feat: upgrade to trpc v10

--- src/providers/ClientProvider.tsx
+++ src/providers/ClientProvider.tsx
@@ -1,9 +1,22 @@
-import * as React from 'react';
-import { QueryClientProvider } from 'react-query';
-import { queryClient, trpc, trpcClient } from '../utils/trpc';
+import React, { useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { trpc } from '../utils/trpc';
+import { httpBatchLink } from '@trpc/client';
 
-export const ClientProvider = ({ children }: React.PropsWithChildren) => (
-  <trpc.Provider client={trpcClient} queryClient={queryClient}>
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  </trpc.Provider>
-);
+export const ClientProvider = ({ children }: React.PropsWithChildren) => {
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: '/trpc'
+        })
+      ]
+    })
+  );
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </trpc.Provider>
+  );
+};
