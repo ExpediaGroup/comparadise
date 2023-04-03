9d821636af6af80f6c151f2f68cd29a7c7abbb8c
Dan Adajian <dadajian@expediagroup.com>
2023-04-03 11:24:44 -0500

    format

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         target: 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
-        ws: true,
+        ws: true
       }
     }
   }
