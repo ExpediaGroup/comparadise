5b20f842ffd69a41c6e280e04c166cf0ae6a3ce9
Dan Adajian <dadajian@expediagroup.com>
2023-05-01 17:22:14 -0500

    cleanup

--- backend/constants.ts
+++ backend/constants.ts
@@ -2,6 +2,5 @@ export const BASE_IMAGES_DIRECTORY = 'base-images';
 export const BASE_IMAGE_NAME = 'base';
 export const DIFF_IMAGE_NAME = 'diff';
 export const NEW_IMAGE_NAME = 'new';
-export const VISUAL_REGRESSION_STATUS_NAME = 'Visual Regression';
 export const UPDATE_BASE_IMAGES_ERROR_MESSAGE =
   'At least one non-visual status check is failing on your PR. Please ensure all other checks are passing before updating base images!';
