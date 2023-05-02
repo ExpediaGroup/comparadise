2c79a2206004fb32e0f8b97879de15a20ee43e31
Dan Adajian <dadajian@expediagroup.com>
2023-05-02 10:54:08 -0500

    tweak error msg

--- backend/constants.ts
+++ backend/constants.ts
@@ -3,4 +3,4 @@ export const BASE_IMAGE_NAME = 'base';
 export const DIFF_IMAGE_NAME = 'diff';
 export const NEW_IMAGE_NAME = 'new';
 export const UPDATE_BASE_IMAGES_ERROR_MESSAGE =
-  'At least one non-visual status check is failing on your PR. Please ensure all other checks are passing before updating base images!';
+  'At least one non-visual status check has not passed on your PR. Please ensure all other checks have passed before updating base images!';
