a844b9f84798f8f06196eeb887b0333583c0cede
Dan Adajian <dadajian@expediagroup.com>
2023-05-02 14:53:56 -0500

    fix: allow base image update when check re-runs succeed

--- backend/allNonVisualChecksHavePassed.test.ts
+++ backend/allNonVisualChecksHavePassed.test.ts
@@ -13,15 +13,18 @@ describe('allNonVisualChecksHavePassed', () => {
               check_runs: [
                 {
                   name: 'unit tests',
-                  conclusion: 'success'
+                  conclusion: 'success',
+                  completed_at: '2023-05-02T19:11:02Z'
                 },
                 {
                   name: 'visual tests',
-                  conclusion: 'failure'
+                  conclusion: 'failure',
+                  completed_at: '2023-05-02T19:11:02Z'
                 },
                 {
                   name: 'other tests',
-                  conclusion: 'success'
+                  conclusion: 'success',
+                  completed_at: '2023-05-02T19:11:02Z'
                 }
               ]
             }
@@ -42,15 +45,18 @@ describe('allNonVisualChecksHavePassed', () => {
               check_runs: [
                 {
                   name: 'unit tests',
-                  conclusion: 'success'
+                  conclusion: 'success',
+                  completed_at: '2023-05-02T19:11:02Z'
                 },
                 {
                   name: 'visual tests',
-                  conclusion: 'failure'
+                  conclusion: 'failure',
+                  completed_at: '2023-05-02T19:11:02Z'
                 },
                 {
                   name: 'other tests',
-                  conclusion: 'failure'
+                  conclusion: 'failure',
+                  completed_at: '2023-05-02T19:11:02Z'
                 }
               ]
             }
@@ -71,15 +77,18 @@ describe('allNonVisualChecksHavePassed', () => {
               check_runs: [
                 {
                   name: 'unit tests',
-                  conclusion: 'success'
+                  conclusion: 'success',
+                  completed_at: '2023-05-02T19:11:02Z'
                 },
                 {
                   name: 'visual tests',
-                  conclusion: 'failure'
+                  conclusion: 'failure',
+                  completed_at: '2023-05-02T19:11:02Z'
                 },
                 {
                   name: 'other tests',
-                  conclusion: 'skipped'
+                  conclusion: 'skipped',
+                  completed_at: '2023-05-02T19:11:02Z'
                 }
               ]
             }
@@ -89,5 +98,79 @@ describe('allNonVisualChecksHavePassed', () => {
     }));
     const result = await allNonVisualChecksHavePassed('github-owner', 'github-repo', 'sha');
     expect(result).toBe(true);
+  });
+
+  it('should return true when a non-visual check failed on an early run but passed on the latest run', async () => {
+    (getOctokit as jest.Mock).mockImplementation(() => ({
+      rest: {
+        checks: {
+          listForRef: jest.fn().mockReturnValue({
+            data: {
+              check_runs: [
+                {
+                  name: 'unit tests',
+                  conclusion: 'failure',
+                  completed_at: '2023-05-02T19:10:02Z'
+                },
+                {
+                  name: 'unit tests',
+                  conclusion: 'success',
+                  completed_at: '2023-05-02T19:11:02Z'
+                },
+                {
+                  name: 'visual tests',
+                  conclusion: 'failure',
+                  completed_at: '2023-05-02T19:11:02Z'
+                },
+                {
+                  name: 'other tests',
+                  conclusion: 'skipped',
+                  completed_at: '2023-05-02T19:11:02Z'
+                }
+              ]
+            }
+          })
+        }
+      }
+    }));
+    const result = await allNonVisualChecksHavePassed('github-owner', 'github-repo', 'sha');
+    expect(result).toBe(true);
+  });
+
+  it('should return false when a non-visual check fails on multiple runs and never passed', async () => {
+    (getOctokit as jest.Mock).mockImplementation(() => ({
+      rest: {
+        checks: {
+          listForRef: jest.fn().mockReturnValue({
+            data: {
+              check_runs: [
+                {
+                  name: 'unit tests',
+                  conclusion: 'failure',
+                  completed_at: '2023-05-02T19:11:02Z'
+                },
+                {
+                  name: 'unit tests',
+                  conclusion: 'failure',
+                  completed_at: '2023-05-02T19:10:02Z'
+                },
+                {
+                  name: 'visual tests',
+                  conclusion: 'failure',
+                  completed_at: '2023-05-02T19:11:02Z'
+                },
+                {
+                  name: 'other tests',
+                  conclusion: 'skipped',
+                  completed_at: '2023-05-02T19:11:02Z'
+                }
+              ]
+            }
+          })
+        }
+      }
+    }));
+    const result = await allNonVisualChecksHavePassed('github-owner', 'github-repo', 'sha');
+    expect(result).toBe(false);
   });
 });
