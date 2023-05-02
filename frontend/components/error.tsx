d9a97c8293d10f0ed6ea3a845b01f3767a326281
Dan Adajian <dadajian@expediagroup.com>
2023-05-02 10:18:49 -0500

    augment error component

--- frontend/components/error.tsx
+++ frontend/components/error.tsx
@@ -6,9 +6,9 @@ export const Error = ({ error }: { error: TRPCClientErrorLike<AppRouter> }) => {
   const { data, message } = error;
   return (
     <div className="text-center m-5">
-      <h1>Error!</h1>
-      <h2>{`Code: ${data?.code}, Status: ${data?.httpStatus}`}</h2>
-      <h3>{message}</h3>
+      <h1 className="text-3xl">Error!</h1>
+      <h2 className="text-xl">{`Code: ${data?.code}, Status: ${data?.httpStatus}`}</h2>
+      <h3 className="text-lg font-bold mt-5 text-red-700">{message}</h3>
     </div>
   );
 };
