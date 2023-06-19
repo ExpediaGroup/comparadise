77659de20ae7f82d9b78212448ba506da81decfb
danadajian <danadajian@gmail.com>
2023-06-19 18:26:14 -0500

    update

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -29,10 +29,7 @@ function HomepageHeader() {
 
 export default function Home(): JSX.Element {
   return (
-    <Layout
-      title="Home"
-      description="Description will go into a meta tag in <head />"
-    >
+    <Layout title="Home">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
