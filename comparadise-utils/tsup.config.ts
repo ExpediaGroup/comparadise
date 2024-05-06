43d4addbf8ed89ac756569fbfb2de6e0ab5aa9e1
danadajian <danadajian@users.noreply.github.com>
2024-05-06 17:40:21 +0000

    chore: committing generated code

--- comparadise-utils/tsup.config.ts
+++ comparadise-utils/tsup.config.ts
@@ -1,8 +1,8 @@
 import { Options } from 'tsup';
 
 export default {
-    clean: true,
-    dts: true,
-    entry: ['index.ts', 'match-screenshot.ts'],
-    noExternal: ['shared']
-} satisfies Options
+  clean: true,
+  dts: true,
+  entry: ['index.ts', 'match-screenshot.ts'],
+  noExternal: ['shared']
+} satisfies Options;
