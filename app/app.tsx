9a8e3882cf94adf1b6da7411d10149f4ed6d63ad
Dan Adajian <dadajian@expediagroup.com>
2025-01-22 17:32:53 -0600

    get frontend working in dev mode

--- app/app.tsx
+++ app/app.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren } from 'react';
+import React from 'react';
 import { MainPage } from './frontend/components/main-page';
 import { ClientProvider } from './frontend/providers/client-provider';
 import { BaseImageStateProvider } from './frontend/providers/base-image-state-provider';
@@ -29,24 +29,5 @@ export function App(props: {
         )}
       </BaseImageStateProvider>
     </ClientProvider>
-  );
-}
-
-export function OuterHtml(props: PropsWithChildren) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" type="image/svg+xml" href="/public/island.svg" />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-        />
-        <link rel="stylesheet" href="/public/globals.css" />
-        <title>Comparadise</title>
-      </head>
-      <body>{props.children}</body>
-    </html>
   );
 }
--- app/backend/src/router.ts
+++ app/backend/src/router.ts
@@ -1,15 +1,10 @@
-import { AnyTRPCRouter, initTRPC } from '@trpc/server';
-import {
-  type FetchHandlerRequestOptions,
-  fetchRequestHandler
-} from '@trpc/server/adapters/fetch';
+import { initTRPC } from '@trpc/server';
 import {
   fetchCurrentPageInputSchema,
   updateBaseImagesInputSchema
 } from './schema';
 import { fetchCurrentPage } from './fetchCurrentPage';
 import { updateBaseImagesInS3 } from './updateBaseImagesInS3';
-import Elysia from 'elysia';
 
 const t = initTRPC.create();
 
@@ -23,47 +18,3 @@ export const router = t.router({
 });
 
 export type AppRouter = typeof router;
-
-type TRPCOptions = {
-  endpoint?: string;
-} & Omit<
-  FetchHandlerRequestOptions<AnyTRPCRouter>,
-  'req' | 'router' | 'endpoint'
->;
-
-export const trpcRouter =
-  (
-    router: AnyTRPCRouter,
-    { endpoint = '/trpc', ...options }: TRPCOptions = { endpoint: '/trpc' }
-  ) =>
-  (app: Elysia) => {
-    return app
-      .onParse(({ request: { url } }) => {
-        if (getPath(url).startsWith(endpoint)) return true;
-      })
-      .get(`${endpoint}/*`, async ({ request }) => {
-        return fetchRequestHandler({
-          ...options,
-          req: request,
-          router,
-          endpoint
-        });
-      })
-      .post(`${endpoint}/*`, async ({ request }) => {
-        return fetchRequestHandler({
-          ...options,
-          req: request,
-          router,
-          endpoint
-        });
-      });
-  };
-
-const getPath = (url: string) => {
-  const start = url.indexOf('/', 9);
-  const end = url.indexOf('?', start);
-
-  if (end === -1) return url.slice(start);
-
-  return url.slice(start, end);
-};
