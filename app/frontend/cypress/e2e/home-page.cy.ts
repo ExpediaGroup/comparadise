45104fef894dc7a0130eace575e8e5ded884ca45
Dan Adajian <dadajian@expediagroup.com>
2025-01-23 10:48:07 -0600

    fix: health check

--- app/frontend/cypress/e2e/home-page.cy.ts
+++ app/frontend/cypress/e2e/home-page.cy.ts
@@ -1,6 +1,11 @@
 describe('Homepage', () => {
   it('should visit the home page', () => {
     cy.visit('/');
-    cy.findByText(/Welcome to Comparadise/);
+    cy.findByText(/Welcome to Comparadise/).should('be.visible');
+  });
+
+  it('should return 200 response for health check', () => {
+    cy.visit('/health');
+    cy.findByText(/healthy/).should('be.visible');
   });
 });
