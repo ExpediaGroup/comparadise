32334b38ebd118fb6a5c97e99c283c23614deac8
Dan Adajian <dadajian@expediagroup.com>
2025-04-07 15:26:44 -0500

    fix: publish comparadise-utils with esmodules

--- comparadise-utils/tsup.config.ts
+++ comparadise-utils/tsup.config.ts
@@ -3,6 +3,8 @@ import { Options } from 'tsup';
 export default {
   clean: true,
   dts: true,
+  format: 'esm',
+  outExtension: () => ({ js: '.js' }),
   entry: ['index.ts', 'match-screenshot.ts'],
   noExternal: ['shared']
 } satisfies Options;
