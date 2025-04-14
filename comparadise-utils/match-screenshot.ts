701b7e5869647286c6a8bac0827d8f757796095c
tmichaels <tmichaels@expediagroup.com>
2025-04-14 14:51:09 -0700

    create takeBaseScreenshot fun

--- comparadise-utils/match-screenshot.ts
+++ comparadise-utils/match-screenshot.ts
@@ -82,6 +82,25 @@ export type MatchScreenshotArgs = {
   };
 };
 
+export function takeBaseScreenshot(
+  subject: Cypress.JQueryWithSelector | Window | Document | void,
+  args?: MatchScreenshotArgs
+) {
+  const { rawName, options } = args || {};
+  // Set up screen
+  forceFont();
+  // Making sure each image is visible before taking screenshots
+  verifyImages();
+
+  const { name, screenshotsFolder } = getTestFolderPathFromScripts(rawName);
+  const target = subject ? cy.wrap(subject) : cy;
+
+  // Create a new updated base image to compare against
+  cy.screenshot(`${PREFIX_DIFFERENTIATOR}${screenshotsFolder}/base`, options);
+
+  cy.task('log', `✅ A new base image was created for ${name}.`);
+}
+
 export function matchScreenshot(
   subject: Cypress.JQueryWithSelector | Window | Document | void,
   args?: MatchScreenshotArgs
@@ -133,6 +152,12 @@ export function matchScreenshot(
     return null;
   });
 }
+
+Cypress.Commands.add(
+  'takeBaseScreenshot',
+  { prevSubject: ['optional', 'element', 'window', 'document'] },
+  takeBaseScreenshot
+);
 
 Cypress.Commands.add(
   'matchScreenshot',
