128c1f404244e40cf59fb2aa2ae34d819860de44
tmichaels <tmichaels@expediagroup.com>
2025-04-22 14:20:37 -0700

    fix issue with generated files not working properly

--- comparadise-utils/tsup.config.ts
+++ comparadise-utils/tsup.config.ts
@@ -3,6 +3,6 @@ import { Options } from 'tsup';
 export default {
   clean: true,
   dts: true,
-  entry: ['index.ts', 'match-screenshot.ts'],
+  entry: ['index.ts', 'match-screenshot.ts', 'create-base-image.ts'],
   noExternal: ['shared']
 } satisfies Options;
