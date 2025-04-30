6c360f6a4db4ec25d0ac37b8eb8ddf140ce616eb
tmichaels2025 <tmichaels2025@users.noreply.github.com>
2025-04-30 17:19:35 +0000

    chore: committing generated code

--- action/dist/main.js
+++ action/dist/main.js
@@ -36095,9 +36095,7 @@ var run = async () => {
     ...import_github6.context.repo
   });
   await createGithubComment();
-  if (numVisualTestFailures > 0) {
-    (0, import_core6.setFailed)("Visual regression differences found while taking a screenshot!");
-  }
+  (0, import_core6.setFailed)("Visual regression differences found while taking a screenshot!");
 };
 
 // src/main.ts
