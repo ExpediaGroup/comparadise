d801647648d03ea0f36bced89a798d90314f9d50
dadajian <dadajian@expediagroup.com>
2025-12-28 10:42:03 -0600

    fix s3 client

--- app/backend/src/s3Client.ts
+++ app/backend/src/s3Client.ts
@@ -3,5 +3,5 @@ import { fromIni } from '@aws-sdk/credential-providers';
 
 // Create S3Client with credentials loaded from ~/.aws/credentials and ~/.aws/config files
 // This automatically picks up credentials, SSO tokens, and handles role assumptions without checking environment variables
-const credentials = await fromIni()();
+const credentials = process.env.CI ? undefined : await fromIni()();
 export const s3Client = new S3Client(credentials);
