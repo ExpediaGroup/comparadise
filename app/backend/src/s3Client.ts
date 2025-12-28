ca88bec5b52848b98ae7cadaf91aab88f72b75be
dadajian <dadajian@expediagroup.com>
2025-12-28 11:14:00 -0600

    fix s3 client

--- app/backend/src/s3Client.ts
+++ app/backend/src/s3Client.ts
@@ -3,5 +3,11 @@ import { fromIni } from '@aws-sdk/credential-providers';
 
 // Create S3Client with credentials loaded from ~/.aws/credentials and ~/.aws/config files
 // This automatically picks up credentials, SSO tokens, and handles role assumptions without checking environment variables
-const credentials = process.env.CI ? undefined : await fromIni()();
+let credentials = {};
+try {
+  credentials = await fromIni()();
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+} catch (error) {
+  console.info('Credentials not found.');
+}
 export const s3Client = new S3Client(credentials);
