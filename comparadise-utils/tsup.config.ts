60a1d7cbecc31278a6a3883d84d864714d1fc2e6
dadajian <dadajian@expediagroup.com>
2026-04-18 19:48:08 -0500

    fix build

--- comparadise-utils/tsup.config.ts
+++
@@ -1,8 +0,0 @@
-import { Options } from 'tsup';
-
-export default {
-  clean: true,
-  dts: true,
-  entry: ['index.ts', 'match-screenshot.ts', 'create-base-image.ts'],
-  noExternal: ['shared']
-} satisfies Options;
